d68727e6192ed7c4922edf1afdeb02f46ac44782
Skyler Burger <skylerburger@gmail.com>
2024-01-05 19:41:13 -0800

    Adds start to tab options

--- src/GroupTabs/GroupTabs.js
+++ src/GroupTabs/GroupTabs.js
@@ -1,34 +1,49 @@
 import './groupTabs.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRotateRight, faUpload } from '@fortawesome/free-solid-svg-icons'
+import { faArrowRotateRight, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons'
 import Settings from '../Settings/Settings';
 
-function GroupTabs({ groups, groupIndex, refreshConfig, setGroupIndex }) {
+
+function GroupTab({ group, idx, isSelected, setGroupIndex }) {
+    function TabOptions() {
+        return (
+            <div className="tabOptions">
+                <FontAwesomeIcon icon={ faEllipsisVertical } /> 
+            </div>
+        )
+    }
+
     function handleClick({ target }) {
         setGroupIndex(target.value);
     }
 
     return (
+        <li 
+        className={ isSelected ? 'selectedGroup' : '' }
+        key={ idx } 
+        value={ idx } 
+        onClick={ handleClick }
+    >
+        { group.groupName }
+        { isSelected ? <TabOptions /> : '' }
+    </li>
+    )
+
+}
+
+function GroupTabs({ groups, groupIndex, refreshConfig, setGroupIndex }) {
+
+    return (
         <div id="GroupTabs">
             <ul>
                 { groups.map( (group, idx) => {
-                    const addClass = groupIndex === idx ? 'selectedGroup' : ''
                     return (
-                        <li 
-                            className={ addClass }
-                            key={ idx } 
-                            value={ idx } 
-                            onClick={ handleClick }
-                        >
-                            { group.groupName }
-                        </li>
+                        <GroupTab group={ group } idx={ idx } isSelected={ groupIndex === idx } setGroupIndex={ setGroupIndex } />
                     );
                   })
                 }
             </ul>
             <ul>
-                <li><FontAwesomeIcon icon={ faUpload } /></li>
-                <li onClick={ refreshConfig }><FontAwesomeIcon icon={ faArrowRotateRight } /></li>
                 <li><Settings /></li>
             </ul>
         </div>
