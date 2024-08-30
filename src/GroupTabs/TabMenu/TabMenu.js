57baba202547c67e45b5e0eb2069c1d223a1f191
Skyler Burger <skylerburger@gmail.com>
2024-08-30 12:22:02 -0700

    ISSU-19 Updates style

--- src/GroupTabs/TabMenu/TabMenu.js
+++ src/GroupTabs/TabMenu/TabMenu.js
@@ -5,13 +5,6 @@ import "./TabMenu.css";
 function TabMenu({ onClose, setShowDetails }) {
   const menuRef = useRef(null);
 
-  // TODO: Remove this
-  const mockFunctionality = (event, name) => {
-    event.stopPropagation();
-    console.log(`${name} was selected`);
-    if (menuRef.current) menuRef.current.blur();
-  };
-
   const openDetails = (event) => {
     event.stopPropagation();
     setShowDetails(true);
@@ -27,9 +20,7 @@ function TabMenu({ onClose, setShowDetails }) {
   return (
     <div ref={menuRef} className="TabMenu" onBlur={onClose} tabIndex={0}>
       <ul>
-        <li onClick={(e) => mockFunctionality(e, "Add Dial")}>Add Dial</li>
         <li onClick={openDetails}>Edit Group</li>
-        <li onClick={(e) => mockFunctionality(e, "New Group")}>New Group</li>
       </ul>
     </div>
   );
