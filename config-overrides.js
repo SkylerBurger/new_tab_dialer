5251ce07dbf85f94fb5d44c156dbbb5149cc8442
Skyler Burger <skylerburger@gmail.com>
2024-09-23 12:31:31 -0700

    Updates format script

--- config-overrides.js
+++ config-overrides.js
@@ -4,7 +4,9 @@ const webpack = require("webpack");
 module.exports = override(
   addWebpackPlugin(
     new webpack.DefinePlugin({
-      "process.env.REACT_APP_ENVIRONMENT": JSON.stringify(process.env.REACT_APP_ENVIRONMENT || "production"),
-    })
-  )
+      "process.env.REACT_APP_ENVIRONMENT": JSON.stringify(
+        process.env.REACT_APP_ENVIRONMENT || "production",
+      ),
+    }),
+  ),
 );
