1070788fce039cf74ba01cfda1067c9038fbd2e2
Skyler Burger <skylerburger@gmail.com>
2024-10-31 09:24:59 -0700

    ISSU-76 Adds drop-shadow indication to placeholder image

--- src/Dial/Dial.js
+++ src/Dial/Dial.js
@@ -3,13 +3,13 @@ import useCachedImage from "../Common/Hooks/useCachedImage";
 function Dial({ icon, name, link, incrementLoadCount }, key) {
   const storageDuration = 1000 * 60 * 60 * 24; // 1 day
   const { error, image } = useCachedImage("dial-images", icon, storageDuration);
-  console.log(error, image);
   const imgTitle = error
     ? `${name} - Error occurred while fetching image`
     : name;
+  const style = error ? { filter: "drop-shadow(0 0 3px rgb(255, 50, 0))" } : {};
 
   return (
-    <a href={link} key={key} title={imgTitle}>
+    <a href={link} key={key} title={imgTitle} style={style}>
       <img src={image} alt={imgTitle} onLoad={incrementLoadCount} />
     </a>
   );
